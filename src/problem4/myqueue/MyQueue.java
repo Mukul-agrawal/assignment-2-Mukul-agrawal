38ee1eba3be7b80cee47af65778ff79e910f98ad
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-03-28 12:32:20 +0530

    create getSize method

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -10,4 +10,21 @@ package problem4.myqueue;
 public class MyQueue {
     private Node front, end, temp;
     private int size;
+
+    public MyQueue() {
+        front = null;
+        end = null;
+        temp = null;
+        size = 0;
+    }
+
+    public int getSize(MyQueue queue) {
+        queue.temp = queue.front;
+        while (queue.temp != null) {
+            ++size;
+            queue.temp = queue.temp.getNext();
+        }
+        queue.temp = queue.front;
+        return size++;
+    }
 }
