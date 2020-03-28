30ddb51caf268acd6353b75633305073ba811959
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-03-28 09:03:20 +0530

    create getter and setter of left

--- src/problem2/main/TreeNode.java
+++ src/problem2/main/TreeNode.java
@@ -11,6 +11,14 @@ public class TreeNode {
         right = null;
     }
 
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
     public int getData() {
         return data;
     }
