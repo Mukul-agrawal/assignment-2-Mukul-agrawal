5c0681a7d77a2269b9077e868d5033575a297fe0
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-03-28 12:27:49 +0530

    create getter and setter methods of node and next

--- src/problem4/myqueue/Node.java
+++ src/problem4/myqueue/Node.java
@@ -4,6 +4,23 @@ import problem2.main.TreeNode;
 
 public class Node {
     private TreeNode node;
+
+    public TreeNode getNode() {
+        return node;
+    }
+
+    public void setNode(TreeNode node) {
+        this.node = node;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
     private Node next;
 
     public Node(int data) {
