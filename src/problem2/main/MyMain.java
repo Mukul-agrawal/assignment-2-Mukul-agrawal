574c5776aeb54593725e9e76b33c73b3ba1949bb
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-03-28 09:22:48 +0530

    write the code in main method

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -7,6 +7,21 @@
 package problem2.main;
 public class MyMain {
     public static void main(String[] args) {
+        PrePostOrder prePostOrder = new PrePostOrder();
+        prePostOrder.insert(40);
+        prePostOrder.insert(50);
+        prePostOrder.insert(30);
+        prePostOrder.insert(35);
+        prePostOrder.insert(100);
+        prePostOrder.insert(90);
+        prePostOrder.insert(12);
+        prePostOrder.insert(34);
+        System.out.println("PreOrder traverse");
+        prePostOrder.traversePreOrder(prePostOrder.getRoot());
+        System.out.println();
+        System.out.println("PostOrder traverse");
+        prePostOrder.traversePostOrder(prePostOrder.getRoot());
+        System.out.println("Both condition are satisfied");
 
     }
 }
