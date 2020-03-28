9ef004541901fcefd86b03f91e22ce304d3190e7
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-03-28 11:14:10 +0530

    create toString method

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -68,4 +68,17 @@ public class MyPriorityQueue<E> implements QueueADT<E> {
         E data = front.getData();
         return data;
     }
+
+    public String toString() {
+        final StringBuilder stringBuilder = new StringBuilder("[");
+        Node<E> temp = front;
+        for (int i = 0; i < size && temp != null; i++) {
+            E data = temp.getData();
+            stringBuilder.append(data);
+            stringBuilder.append((i < size - 1) ? ",\n" : "");
+            temp = temp.getNext();
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
 }
