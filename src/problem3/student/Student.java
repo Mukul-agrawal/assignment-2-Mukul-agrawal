879dabd517b0e8274fd61a26e062883ed164c29a
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-03-28 11:17:48 +0530

    create Student class constructor

--- src/problem3/student/Student.java
+++ src/problem3/student/Student.java
@@ -3,4 +3,13 @@ package problem3.student;
 public class Student {
     private int rollNumber;
     private String name;
+
+    public Student(int rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+    }
+
+    public Student(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
 }
