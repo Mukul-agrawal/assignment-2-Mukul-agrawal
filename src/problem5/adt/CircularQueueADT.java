67b19a21ccf8cec894ff8bec6595472eaa9db021
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-03-28 17:14:39 +0530

    create methods in CircularQueueADT interface

--- src/problem5/adt/CircularQueueADT.java
+++ src/problem5/adt/CircularQueueADT.java
@@ -1,4 +1,13 @@
 package problem5.adt;
 
-public interface CircularQueueADT {
+public interface CircularQueueADT<E> {
+    public void enqueue(E data);
+
+    public E dequeue();
+
+    public E peek();
+
+    public boolean isEmpty();
+
+    public int getsize();
 }
