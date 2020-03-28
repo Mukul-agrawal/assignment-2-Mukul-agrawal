aab493361e4b27d2ee90f875924a63cd7994ffa1
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-03-28 08:23:34 +0530

    create print method

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -49,4 +49,9 @@ public class MyBinarySearchTree<E> implements MyBinarysearchTreeADT<E> {
         printLeftChild(node.getLeftChild(), level + 1);
         printLeftChild(node.getRightChild(), level + 1);
     }
+
+    public void print() {
+        printLeftChild(root, 1);
+        System.out.println();
+    }
 }
