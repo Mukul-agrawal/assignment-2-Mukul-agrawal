1b0620f9d8bb9c4c64fc17a337391eea5edda923
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-03-28 08:07:51 +0530

    create private TreeNode<E> root

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -6,8 +6,11 @@
  */
 package problem1.mybst;
 
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree<E> implements MyBinarysearchTreeADT<E> {
+    private TreeNode<E> root;
 
     @Override
     public boolean add(E data) {
