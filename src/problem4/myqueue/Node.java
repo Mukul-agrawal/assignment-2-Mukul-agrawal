dd6078f05bdd4d2d5939ec5bd932115b9d785e80
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-03-28 12:26:59 +0530

    create Node constructor

--- src/problem4/myqueue/Node.java
+++ src/problem4/myqueue/Node.java
@@ -5,4 +5,14 @@ import problem2.main.TreeNode;
 public class Node {
     private TreeNode node;
     private Node next;
+
+    public Node(int data) {
+        node = new TreeNode(data);
+        next = null;
+    }
+
+    public Node(TreeNode treeNode) {
+        node = treeNode;
+        next = null;
+    }
 }
