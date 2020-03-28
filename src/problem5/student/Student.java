0501f4f1bcbafd9c797741e2b442577e18e5bc4f
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-03-28 17:09:10 +0530

    Did some changes

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -6,25 +6,11 @@
  */
 package problem5.student;
 
-import java.util.Scanner;
-
 // to store student information and properties
 public class Student {
     private String name;
-    private int rollNo, backlog, appearingCount;
-    private Scanner scanner;
-
-    public Student() {
-        scanner = new Scanner(System.in);
-        System.out.print("Name : ");
-        this.name = scanner.next();
-        System.out.print("Roll no : ");
-        this.rollNo = scanner.nextInt();
-        System.out.print("Backlog_Count : ");
-        this.backlog = scanner.nextInt();
-        System.out.print("Appearing_Count : ");
-        this.appearingCount = scanner.nextInt();
-    }
+    private int rollNo;
+    private int backlog;
 
     public String getName() {
         return name;
@@ -50,21 +36,4 @@ public class Student {
         this.backlog = backlog;
     }
 
-    public int getAppearingCount() {
-        return appearingCount;
-    }
-
-    public void setAppearingCount(int appearingCount) {
-        this.appearingCount = appearingCount;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "name='" + name + '\'' +
-                ", rollNo=" + rollNo +
-                ", backlog=" + backlog +
-                ", appearingCount=" + appearingCount +
-                '}';
-    }
 }
